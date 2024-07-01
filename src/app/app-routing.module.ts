74742e63cef357c0f2ec484ddab2a81ac39c14af
HenriqueDiasSilva <henriquediasilv1@gmail.com>
2024-07-01 11:16:33 -0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'creditcards', loadChildren: () => import('./creditcards/creditcards.module').then(m => m.CreditcardsModule) },
+  { path: '**', redirectTo: 'creditcards' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
