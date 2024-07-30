1333dd6bcf5ab59469192db39a0055ebbb8ed61f
HenriqueDiasSilva <henriquediasilv1@gmail.com>
2024-07-29 21:26:50 -0300

    feat: Adiciona obtenção de id via URL e visualização dinâmica de cada credit card

--- src/app/creditcards/view/view.component.ts
+++ src/app/creditcards/view/view.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { CreditcardsService } from '../../services/creditcards.service';
 import { CreditCard } from 'src/app/models/credit-card';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
-  styleUrls: ['./view.component.scss']
+  styleUrls: ['./view.component.scss'],
 })
 export class ViewComponent {
-
   creditCardDetails!: CreditCard;
+  creditCardId!: Number;
 
-  constructor(private creditcardsService: CreditcardsService) {
-    this.creditcardsService.getCreditCardById(3).subscribe((data: CreditCard) => {
-      this.creditCardDetails = data;
-    });
+  constructor(
+    private creditcardsService: CreditcardsService,
+    private route: ActivatedRoute
+  ) {
+
+    this.creditCardId = parseInt(this.route.snapshot.paramMap.get('id') || '');
+
+    this.creditcardsService
+      .getCreditCardById(this.creditCardId)
+      .subscribe((data: CreditCard) => {
+        this.creditCardDetails = data;
+      });
   }
 }
