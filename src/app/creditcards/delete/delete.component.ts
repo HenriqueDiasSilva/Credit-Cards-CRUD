9cd1b993a31f209ea1782966fece63533eccf8c6
HenriqueDiasSilva <henriquediasilv1@gmail.com>
2024-07-29 21:36:00 -0300

    feat: feat: Adiciona a funcionalidade de deletar um credit card pelo ID

--- src/app/creditcards/delete/delete.component.ts
+++ src/app/creditcards/delete/delete.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { CreditCard } from 'src/app/models/credit-card';
+import { CreditcardsService } from '../../services/creditcards.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-delete',
   templateUrl: './delete.component.html',
-  styleUrls: ['./delete.component.scss']
+  styleUrls: ['./delete.component.scss'],
 })
 export class DeleteComponent {
+  creditCardDetails!: CreditCard;
+  creditCardId!: Number;
 
+  constructor(
+    private creditcardsService: CreditcardsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.creditCardId = parseInt(this.route.snapshot.paramMap.get('id') || '');
+
+    this.creditcardsService
+      .deleteCreditCard(this.creditCardId)
+      .subscribe(() => {
+        alert('Credit card deleted...');
+        this.router.navigate(['creditcards']);
+      });
+  }
 }
