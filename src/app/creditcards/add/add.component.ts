24f0879d9eafa477082a6079ac4fe8cf5e0903a1
HenriqueDiasSilva <henriquediasilv1@gmail.com>
2024-08-23 09:54:31 -0300

    feat: Finaliza a funcionalidade de criar um credit card

--- src/app/creditcards/add/add.component.ts
+++ src/app/creditcards/add/add.component.ts
@@ -1,28 +1,35 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CreditCard } from 'src/app/models/credit-card';
+import { CreditcardsService } from 'src/app/services/creditcards.service';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent {
+  constructor(private creditCardsService:CreditcardsService, private router:Router) {}
 
   newCreditCard: CreditCard = {
     id: undefined,
-    cardName: "",
-    bankName: "",
-    description: "",
+    cardName: '',
+    bankName: '',
+    description: '',
     maxCredit: 5000,
     active: false,
     annualFee: 12,
     interestRate: 12,
-    recommededCreditScore: "100-500",
+    recommededCreditScore: '100-500',
     createdDate: Date(),
-    termsAndConditions: "Terms and conditions for the credit card",
-  }
+    termsAndConditions: 'Terms and conditions for the credit card',
+  };
 
-  saveCreditCard(){
+  saveCreditCard() {
     console.log(this.newCreditCard);
+    this.creditCardsService.createCreditCard(this.newCreditCard).subscribe(data => {
+      alert("Create New Credit Card");
+      this.router.navigate(['creditcards'])
+    });
   }
 }
